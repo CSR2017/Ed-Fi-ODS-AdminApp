1c66d40f2b7f802096da02f75379a864d4332c4c
Suganya <srajendran@certicasolutions.com>
2020-12-09 14:41:26 -0600

    Add nuget feed credentials to build features

--- .teamcity/_Self/buildTypes/PublishPackageToAzure.kt
+++ .teamcity/_Self/buildTypes/PublishPackageToAzure.kt
@@ -9,6 +9,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.nuGetFeedCredentials
 
 object PublishPackageToAzure : BuildType ({
     name = "Publish Package to Azure"
@@ -25,6 +26,14 @@ object PublishPackageToAzure : BuildType ({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    features {
+        nuGetFeedCredentials {
+            feedUrl = "%azureArtifacts.feed.nuget%"
+            username = "%azureArtifacts.edFiBuildAgent.userName%"
+            password = "%azureArtifacts.edFiBuildAgent.accessToken%"
+        }
     }
 
     steps {
